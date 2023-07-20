05d9a9da96278d320baa4699b7b47745681e0d32
tanquekoin <santiago.silva@koin.com.br>
2023-07-20 16:50:26 -0300

    chore: update price page

--- components/pricing-tables.tsx
+++ components/pricing-tables.tsx
@@ -26,7 +26,7 @@ export default function PricingTables() {
           {/* Pricing tables */}
           <div>
             {/* Pricing toggle */}
-            <div
+            {/* <div
               className="flex justify-center max-w-xs m-auto mb-8"
               data-aos="zoom-y-out"
               data-aos-delay="300"
@@ -65,12 +65,12 @@ export default function PricingTables() {
                   Bill Monthly
                 </button>
               </div>
-            </div>
+            </div> */}
 
-            <div className="max-w-sm md:max-w-2xl xl:max-w-none mx-auto grid gap-8 md:grid-cols-1 xl:grid-cols-1 xl:gap-6 items-start justify-items-center">
+            <div className="max-w-sm md:max-w-2xl xl:max-w-none mx-auto grid gap-8 md:grid-cols-1 xl:grid-cols-3 xl:gap-6 items-start justify-items-center">
               {/* Pricing table 1 */}
-              {/* <div
-                className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl"
+              <div
+                className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl max-w-sm w-full"
                 data-aos="zoom-y-out"
                 data-aos-delay="450"
               >
@@ -94,7 +94,7 @@ export default function PricingTables() {
                     >
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>Unlimited viewers</span>
+                    <span>Unlimited essays</span>
                   </li>
                   <li className="flex items-center mb-2">
                     <svg
@@ -104,7 +104,7 @@ export default function PricingTables() {
                     >
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>Up to 3 members</span>
+                    <span>Up to 300 words</span>
                   </li>
                   <li className="flex items-center mb-2">
                     <svg
@@ -116,16 +116,6 @@ export default function PricingTables() {
                     </svg>
                     <span>Up to 2 projects</span>
                   </li>
-                  <li className="flex items-center mb-2">
-                    <svg
-                      className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0"
-                      viewBox="0 0 12 12"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                    </svg>
-                    <span>Dedicated support</span>
-                  </li>
                 </ul>
                 <div className="border-t border-gray-200 pt-5 mt-6">
                   <a
@@ -135,34 +125,22 @@ export default function PricingTables() {
                     Try for free
                   </a>
                 </div>
-              </div> */}
-
-              {/* Pricing table 2 */}
-              {/* <div
-                className="relative flex flex-col h-full py-5 px-6 rounded bg-blue-100 shadow-xl border-2 border-blue-500"
+              </div>
+              {/* {pricing monthly} */}
+              <div
+                className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl max-w-sm w-full"
                 data-aos="zoom-y-out"
                 data-aos-delay="450"
               >
-                <div className="absolute top-0 right-0 mr-5 p-3 -mt-5 bg-yellow-500 rounded-full">
-                  <svg
-                    className="w-4 h-4 fill-current text-white"
-                    viewBox="0 0 16 16"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M15.145 5.05l-4.316-.627L8.898.513c-.338-.684-1.456-.684-1.794 0l-1.93 3.91-4.317.627a1.002 1.002 0 00-.554 1.707l3.124 3.044-.737 4.3a1 1 0 001.45 1.053L8 13.125l3.862 2.03c.728.381 1.59-.234 1.45-1.054l-.736-4.299L15.7 6.758a1.003 1.003 0 00-.555-1.708z" />
-                  </svg>
-                </div>
                 <div className="mb-4">
-                  <div className="text-lg font-bold mb-1">Agency</div>
+                  <div className="text-lg font-bold mb-1">Enterprise</div>
                   <div className="inline-flex items-baseline mb-2">
                     <span className="text-3xl font-bold">$</span>
-                    <span className="text-4xl font-bold">
-                      {annual ? "24" : "34"}
-                    </span>
+                    <span className="text-4xl font-bold">14</span>
                     <span className="text-gray-600 pl-2">/month</span>
                   </div>
                   <div className="text-lg text-gray-800">
-                    For larger teams that need to create, and collaborate.
+                    Revolutionize your learning
                   </div>
                 </div>
                 <ul className="text-gray-600 -mb-2 grow">
@@ -174,7 +152,7 @@ export default function PricingTables() {
                     >
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>Unlimited viewers</span>
+                    <span>Save hours of boring manual research</span>
                   </li>
                   <li className="flex items-center mb-2">
                     <svg
@@ -184,7 +162,7 @@ export default function PricingTables() {
                     >
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>Up to 3 members</span>
+                    <span>Directly visit the sources</span>
                   </li>
                   <li className="flex items-center mb-2">
                     <svg
@@ -194,7 +172,9 @@ export default function PricingTables() {
                     >
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>Up to 2 projects</span>
+                    <span>
+                      Ensure that the sources used are accurate and relevant
+                    </span>
                   </li>
                   <li className="flex items-center mb-2">
                     <svg
@@ -204,7 +184,39 @@ export default function PricingTables() {
                     >
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>Dedicated support</span>
+                    <span>Instant access to a vast range of sources</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <svg
+                      className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0"
+                      viewBox="0 0 12 12"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>
+                      Quickly find credible sources for your essays and projects
+                    </span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <svg
+                      className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0"
+                      viewBox="0 0 12 12"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Access to every publicly available source</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <svg
+                      className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0"
+                      viewBox="0 0 12 12"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Paste your entire essay</span>
                   </li>
                 </ul>
                 <div className="border-t border-gray-200 pt-5 mt-6">
@@ -212,14 +224,11 @@ export default function PricingTables() {
                     className="btn-sm text-white bg-blue-600 hover:bg-blue-700 w-full"
                     href="#0"
                   >
-                    Start free trial
+                    Buy now
                   </a>
                 </div>
-              </div> */}
-
+              </div>
               {/* Pricing table 3 */}
-
-              {/* Pricing table 4 */}
               <div
                 className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl max-w-sm w-full"
                 data-aos="zoom-y-out"
@@ -229,18 +238,12 @@ export default function PricingTables() {
                   <div className="text-lg font-bold mb-1">Enterprise</div>
                   <div className="inline-flex items-baseline mb-2">
                     <span className="text-3xl font-bold">$</span>
-                    <span className="text-4xl font-bold">
-                      {annual ? "134" : "14"}
-                    </span>
-                    <span className="text-gray-600 pl-2">
-                      {" "}
-                      {annual ? "/year" : "/month"}
-                    </span>
+                    <span className="text-4xl font-bold">134</span>
+                    <span className="text-gray-600 pl-2">/year</span>
+                    <span className="text-green-500 ml-2">-25%</span>
                   </div>
                   <div className="text-lg text-gray-800">
-                    {annual
-                      ? "Maximize your academic potential"
-                      : "Revolutionize your learning"}
+                    Maximize your academic potential
                   </div>
                 </div>
                 <ul className="text-gray-600 -mb-2 grow">
